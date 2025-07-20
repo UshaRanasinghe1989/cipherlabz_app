54645e8cd71e9fe229b72342bf4ac459c1c81a53
UshaRanasinghe1989 <usha.ranasinghe@gmail.com>
2025-07-21 00:28:03 +0530

    Color package updated

--- packages/color_package/lib/src/app_colors.dart
+++ packages/color_package/lib/src/app_colors.dart
@@ -5,21 +5,30 @@ class AppColors {
   static const Color white = Color(0XFFFFFFFF); //#FFFFFF
   static const Color black = Color(0X00000000); //#000000
 
-  static const pageLabelColor = Color.fromRGBO(5, 164, 255, 0.67); //#05A4FFAB
-  static const selectedIconColor = Color(0XFF0051FF);
-  static const dropShadowColor = Color.fromRGBO(0, 0, 0, 0.25);
-  static const poweredByTextColor = Color(0XFF7C7A82); //#7C7A82
+  static const blue05A4FFAB = Color.fromRGBO(5, 164, 255, 0.67); //#05A4FFAB
+  static const blue0051FF = Color(0XFF0051FF); //#0051FF
+  static const gray7C7A82 = Color(0XFF7C7A82); //#7C7A82
 
   //SPLASH SCREEN COLORS
-  static const Color hrManagementTxtBlue = Color(0XFF3700FF); //#3700FF
-  static const Color hrManagementTxtRed = Color(0XFFFF0000); //#FF0000
+  static const Color blue3700FF = Color(0XFF3700FF); //#3700FF
+  static const Color red = Color(0XFFFF0000); //#FF0000
 
   //LOGIN PAGE COLORS
-  static const backgroundBlue = Color(0XFF46AEFC); //#46AEFC
-  static const darkBlue = Color(0xFF001645);
+  static const blue46AEFC = Color(0XFF46AEFC); //#46AEFC
+  static const blue001645 = Color(0xFF001645); //#001645
 
-  static const textFieldFillColor = Color(0XFFFFFAFA);
-  static const hintTextColor = Color.fromRGBO(0, 0, 0, 0.44);
+  static const whiteFFFAFA = Color(0XFFFFFAFA); //#FFFAFA
+  static const grey00000070 = Color.fromRGBO(0, 0, 0, 0.44); //#00000070
 
-  static const signInBtnColor = Color(0XFF0590DF); //#0590DF
+  static const blue0590DF = Color(0XFF0590DF); //#0590DF
+
+  //HOME SCREEN COLORS
+  static const blue0085FF = Color(0XFF0085FF); //#0085FF
+
+  static const pinkE40079 = Color(0XFFE40079); //#E40079
+  static const pinkE400790F = Color.fromRGBO(228, 0, 121, 0.06); //#E400790F
+
+  //ATTENDANCE COLORS
+  static const black00000040 = Color.fromRGBO(0, 0, 0, 0.25); //#00000040
+  static const whiteFFFFFFD4 = Color.fromRGBO(225, 225, 225, 0.83); //#FFFFFFD4
 }

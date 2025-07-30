114111949a02433e0395ca0bcf44063920c55d28
UshaRanasinghe1989 <usha.ranasinghe@gmail.com>
2025-07-30 08:25:30 +0530

    HR app updated

--- apps/hr_app/lib/main.dart
+++ apps/hr_app/lib/main.dart
@@ -1,99 +1,14 @@
-import 'package:attendance_package/attendance_package.dart';
-import 'package:core/helpers/get_location.dart';
-import 'package:core/helpers/local_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 //RESOURCES
 import 'package:shared_resources/shared_resources.dart';
-//CUSTOM PACKAGES
-import 'package:announcement_package/announcement_package.dart';
-import 'package:schedule_package/schedule_package.dart';
 //PAGES
 import 'package:splash/splash.dart';
 import 'package:login_package/login_package.dart';
 
 void main() {
   //runApp(const MyApp());
-  runApp(
-    MultiProvider(
-      providers: [
-        // 1. Provide AttendanceRepository
-        Provider<AttendanceRepository>(
-          create: (_) => AttendanceRepositoryImpl.repo(),
-        ),
-        // 2. Provide AttendanceLocalDataSource
-        Provider<AttendanceLocalDataSource>(
-          create: (_) => AttendanceLocalDataSource(),
-        ),
-        // 3. Provide LocalAuth
-        Provider<LocalAuth>(create: (_) => LocalAuth()),
-        // 4. Provide GetCurrentLoaction
-        Provider<GetCurrentLoaction>(create: (_) => GetCurrentLoaction()),
-        ProxyProvider<AttendanceRepository, IsCheckedInUseCase>(
-          update: (_, repo, __) => IsCheckedInUseCase(repo),
-        ),
-        // 5. Provide SaveAttendanceUseCase using repository, auth, location
-        ProxyProvider3<
-          AttendanceRepository,
-          LocalAuth,
-          GetCurrentLoaction,
-          SaveAttendanceUseCase
-        >(
-          update: (_, repo, auth, location, __) =>
-              SaveAttendanceUseCase(repo, auth, location),
-        ),
-
-        // 6. Provide GetAttendanceUseCase
-        ProxyProvider<AttendanceRepository, GetAttendanceUseCase>(
-          update: (_, repo, __) => GetAttendanceUseCase(repository: repo),
-        ),
-        //PROVIDE GET MY ATTENDANCE USECASE
-        ProxyProvider<AttendanceRepository, GetMyAttendanceUseCase>(
-          update: (_, repo, __) => GetMyAttendanceUseCase(repository: repo),
-        ),
-
-        // 7. Provide AttendanceUseCases grouped
-        ProxyProvider4<
-          SaveAttendanceUseCase,
-          GetAttendanceUseCase,
-          IsCheckedInUseCase,
-          GetMyAttendanceUseCase,
-          AttendanceUseCases
-        >(
-          update:
-              (
-                _,
-                saveAttendance,
-                getAttendance,
-                isCheckedIn,
-                getMyAttendance,
-                __,
-              ) => AttendanceUseCases(
-                saveAttendance: saveAttendance,
-                getAttendanceObj: getAttendance,
-                isCheckedIn: isCheckedIn,
-                getMyAttendance: getMyAttendance,
-              ),
-        ),
-
-        // 8. Provide AttendanceProvider using everything
-        ChangeNotifierProxyProvider3<
-          AttendanceRepository,
-          AttendanceLocalDataSource,
-          AttendanceUseCases,
-          AttendanceProvider
-        >(
-          create: (_) => AttendanceProvider.empty(), // safe placeholder
-          update: (_, repo, dataSource, useCases, __) =>
-              AttendanceProvider(repo, dataSource, useCases),
-        ),
-        ChangeNotifierProvider(create: (context) => LoginProvider()),
-        ChangeNotifierProvider(create: (context) => ScheduleProvider()),
-        ChangeNotifierProvider(create: (context) => AnnouncementProvider()),
-      ],
-      child: const MyApp(),
-    ),
-  );
+  runApp(ProviderScope(child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
